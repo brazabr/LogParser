e74605dcf85ce95d12999af517d39ddf17959765
Thiago Benvenuto <brazabr@users.noreply.github.com>
2022-04-13 15:53:03 -0300

    Fixed typo

--- LogParser/Program.cs
+++ LogParser/Program.cs
@@ -1,12 +1,11 @@
 ﻿using System.IO;
 using System.Text.RegularExpressions;
 
-const string regExpression = @"^(?<horaio>[0-9:]+)\sKill:\s[0-9\s]+:\s(?<quem_matou>[\<\>\w]+)\skilled\s(?<quem_morreu>[\w\s]+)\sby\s(?<causa_morte>\w+)$";
+const string regExpression = @"^(?<horario>[0-9:]+)\sKill:\s[0-9\s]+:\s(?<quem_matou>[\<\>\w]+)\skilled\s(?<quem_morreu>[\w\s]+)\sby\s(?<causa_morte>\w+)$";
 
 var allLines = File.ReadAllLines("Quake.txt");
 
 var total_kills = 0;
-var players = new Dictionary<string, int>();
 
 foreach (var line in allLines)
 {
@@ -20,7 +19,11 @@ foreach (var line in allLines)
         var causa_morte = parsedLine.Groups["causa_morte"].Value;
 
         Console.WriteLine("{0} matou {1} usando {2} no horário {3}", quem_matou, quem_morreu, causa_morte, horario);
+
+        total_kills++;
     }
 }
+
+Console.WriteLine("{0} mortes no total", total_kills);
 
 Console.ReadKey();
