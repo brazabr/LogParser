1592efc426c1406f7af2b67931954e1012610c19
Thiago Benvenuto <brazabr@users.noreply.github.com>
2022-04-13 15:50:29 -0300

    Add project files.

---
+++ LogParser/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Text.RegularExpressions;
+
+const string regExpression = @"^(?<horaio>[0-9:]+)\sKill:\s[0-9\s]+:\s(?<quem_matou>[\<\>\w]+)\skilled\s(?<quem_morreu>[\w\s]+)\sby\s(?<causa_morte>\w+)$";
+
+var allLines = File.ReadAllLines("Quake.txt");
+
+var total_kills = 0;
+var players = new Dictionary<string, int>();
+
+foreach (var line in allLines)
+{
+    var parsedLine = Regex.Match(line, regExpression);
+
+    if (parsedLine.Success)
+    {
+        var horario = parsedLine.Groups["horario"].Value;
+        var quem_matou = parsedLine.Groups["quem_matou"].Value;
+        var quem_morreu = parsedLine.Groups["quem_morreu"].Value;
+        var causa_morte = parsedLine.Groups["causa_morte"].Value;
+
+        Console.WriteLine("{0} matou {1} usando {2} no horário {3}", quem_matou, quem_morreu, causa_morte, horario);
+    }
+}
+
+Console.ReadKey();
